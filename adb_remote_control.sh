2149eba6392e45e3ebc620d46406ee4787d7e6e5
YakusaFire <64071544+YakusaFire@users.noreply.github.com>
2026-04-08 15:19:57 +0000

    Add files via upload

--- adb_remote_control.sh
+++ adb_remote_control.sh
@@ -2,30 +2,30 @@
 
 # YakusaFire
 
-a=0
+key=0
 echo "---"
 echo "Taper '666' pour fermer la télécommande"
 echo "---"
 echo "Taper '501' pour obtenir les numéros de KeyEvent"
 echo "---"
 
-while [ $a -ne 666 ]; do
+while [ $key -ne 666 ]; do
 
     read -p "Rentré le numéro de KeyEvent à executer: " a >&2
 
     if [[ ! $a =~ ^[0-9]+$ ]]; then
-        echo "Rentré un nombre entier positif"
+        echo "Rentrer un nombre entier positif"
         echo ""
-        a=0
+        key=0
         continue
     fi
 
-    if [ $a -eq "501" ]; then
+    if [ $key -eq "501" ]; then
         cat keyevent.txt
         echo ""
     fi
 
-    adb shell input keyevent $a
+    adb shell input keyevent $key
     echo ""
 
 done
