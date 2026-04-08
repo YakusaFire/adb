323bf1a88a69be1cc8470f7823ee5f63b33ad826
YakusaFire <64071544+YakusaFire@users.noreply.github.com>
2026-04-08 06:28:04 +0000

    Add files via upload

--- adb_save.sh
+++ adb_save.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
-DESTINATION="$HOME/Documents/PROJET_ADB"
+# YakusaFire
+
+DESTINATION="$HOME/Documents/ADB"
 DATE=$(date +%Y-%m-%d_%Hh%M)
 DOSSIER_FINAL="$DESTINATION/backup_$DATE"
 
-echo "--- Sauvegarde ---"
+echo "--- Backup ---"
 
 mkdir -p "$DOSSIER_FINAL"
 
-# Liste des dossiers à sauvegarder (ceux qui sont accessibles)
+# List of folder, we need to save
 DOSSIERS=("DCIM" "Pictures" "Download" "Music" "Movies" "Documents")
 
 for folder in "${DOSSIERS[@]}"; do
-    echo "Copie de : $folder..."
-    # On crée le sous-dossier sur le PC pour que ce soit propre
+    echo "Copy : $folder..."
+    # We create the new folder
     mkdir -p "$DOSSIER_FINAL/$folder"
-    # On pull le contenu
+    # We pull
     adb pull "/sdcard/$folder/." "$DOSSIER_FINAL/$folder/"
 done
 
 echo "---"
-echo "Sauvegarde terminée (les dossiers protégés ont été ignorés)."
-echo "Tes fichiers sont ici : $DOSSIER_FINAL"
+echo "Backup complete"
+echo "Your files are here : $DOSSIER_FINAL"
