eb690b038abd7b98a68f6b371133494609a9e9ef
YakusaFire <64071544+YakusaFire@users.noreply.github.com>
2026-04-08 14:01:34 +0000

    Add files via upload

--- adb_clean.sh
+++ adb_clean.sh
@@ -5,28 +5,46 @@
 # List of packages to be removed
 APPS=(
     # --- Social Networks ---
-    "com.facebook.katana"        # Facebook
-    "com.facebook.system"        # Service FB
-    "com.facebook.appmanager"    # Gestionnaire FB
-    "com.facebook.services"      # Services FB
-    "com.linkedin.android"       # LinkedIn
-    "com.microsoft.skydrive"     # OneDrive
-    "com.netflix.mediaclient"    # Netflix
+    "com.facebook.katana"       			 	# Facebook
+    "com.facebook.system"        				# Service FB
+    "com.facebook.appmanager"    				# Gestionnaire FB
+    "com.facebook.services"      				# Services FB
+    "com.linkedin.android"      			 	# LinkedIn
+    "com.microsoft.skydrive"     				# OneDrive
+    "com.netflix.mediaclient"    				# Netflix
+	"com.microsoft.skype.teams"					# Skype
+	"com.amazon.mShop.android.shopping"			# Amazon
     
     # --- Samsung Bloatware ---
-    "com.samsung.android.bixby.agent"      # Bixby
-    "com.samsung.android.app.spage"        # Samsung Daily / Free
-    "com.samsung.android.kidsinstaller"    # Mode Enfants
-    "com.samsung.android.app.tips"         # Astuces Samsung
-    "com.samsung.android.app.watchmanagerstub" # Galaxy Watch (si tu n'en as pas)
-    "com.samsung.android.messaging"        # SMS Samsung (si tu utilises Google Messages)
-    "com.sec.android.app.sbrowser"         # Navigateur Samsung (si tu utilises Chrome)
-    "com.samsung.android.wellbeing"        # Bien-être numérique (optionnel)
+    "com.samsung.android.bixby.agent"      		# Bixby
+	"com.samsung.android.bixby.wakeup"
+	"com.samsung.android.spay"
+	"com.samsung.android.kidsinstaller"
+    "com.samsung.android.app.spage"        		# Samsung Daily / Free
+    "com.samsung.android.kidsinstaller"    		# Child Mode
+    "com.samsung.android.app.tips"         		# Samsung Tips
+    "com.samsung.android.app.watchmanagerstub" 	# Galaxy Watch
+    "com.samsung.android.messaging"        		# SMS Samsung
+    "com.sec.android.app.sbrowser"         		# Browser Samsung
+    "com.samsung.android.wellbeing"        		# Digital wellbeing
     
     # --- Game ---
-    "com.samsung.android.game.gamehome"    # Game Launcher
-    "com.samsung.android.game.gametools"   # Game Tools
-    "com.samsung.android.game.gos"         # Game Optimizing Service (parfois controversé)
+    "com.samsung.android.game.gamehome"    		# Game Launcher
+    "com.samsung.android.game.gametools"   		# Game Tools
+    "com.samsung.android.game.gos"         		# Game Optimizing Service (parfois controversé)
+
+	# --- Xiaomi ---
+	"com.miui.analytics"
+	"com.miui.msa.global"
+	"com.miui.videoplayer"
+	"com.miui.player"
+	"com.xiaomi.joyose"
+
+	# --- Google ---
+	"com.google.android.apps.tachyon"
+	"com.google.android.videos"
+	"com.google.android.music"
+	"com.google.android.apps.magazines"
 )
 
 echo "--- Start Of Cleaning ---"
