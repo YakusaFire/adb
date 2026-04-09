201a54f837b8d7a5cee45e8153a986a4a84fd446
YakusaFire <choupy.guery@gmail.com>
2026-04-09 07:12:49 +0000

    Update

--- adb_remote_control.sh
+++ adb_remote_control.sh
@@ -2,12 +2,16 @@
 
 # YakusaFire
 
+
 key=0
 echo "---"
 echo "Taper '666' pour fermer la télécommande"
 echo "---"
 echo "Taper '501' pour obtenir les numéros de KeyEvent"
 echo "---"
+echo "Taper '1' pour déverouiller le téléphone"
+echo "---"
+
 
 while [ $key -ne 666 ]; do
 
@@ -24,6 +28,20 @@ while [ $key -ne 666 ]; do
         cat keyevent.txt
         echo ""
     fi
+
+    if [ $key -eq "1" ]; then
+
+        adb shell input keyevent 224
+        adb shell input swipe 500 1500 500 500 200
+        read -p "Rentré le mot de passe: " mdp >&2
+        adb shell input text "$mdp"
+    fi
+
+    if [ $key -eq "502" ]; then
+
+        ./adb_clean.sh
+    fi
+
 
     adb shell input keyevent $key
     echo ""
